4f8eb64842c1e5c37bff9a5898bd8c32753dc422
yeonghoonPark <huklmnj2@naver.com>
2025-02-01 15:43:03 +0900

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import './i18n/i18n.ts';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app/App.tsx';
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
